39a4eb7da25b035da4bbaf1e0c2ffb9f7b265bfb
ValeriaGart <valeria20work@gmail.com>
2024-03-10 15:05:16 +0100

    ex00

---
+++ CPP00/ex00/megaphone.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cctype>
+#include <string>
+
+int main(int ac, char **av)
+{
+	int	i;
+
+	i = 1;
+	if (ac == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+	{
+		while (i < ac)
+		{
+			if (i != 1)
+				std::cout << " ";
+			std::string arg = av[i];
+        	for (char& c : arg)
+            	c = std::toupper(c);
+			std::cout << arg;
+			i++;
+		}
+		std::cout << std::endl;
+	}
+	return 0;
+}

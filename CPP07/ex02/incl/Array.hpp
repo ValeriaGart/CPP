cd2f33fdc0f0bebc9b1638bec8789f85f4f1a435
Valeriia Harkusha <vharkush@c3r7p4.42vienna.com>
2024-05-27 14:45:04 +0200

    CPP07 done

---
+++ CPP07/ex02/incl/Array.hpp
@@ -0,0 +1,74 @@
+#ifndef ARRAY_HPP
+# define ARRAY_HPP
+
+# include <iostream>
+# include <exception>
+
+template <typename T>
+class Array {
+private:
+	unsigned int 	_size;
+	T*				_arr;
+public: 
+	Array( void );
+	Array(const unsigned int n);
+	Array(const Array& other);
+	Array& operator=(const Array& other);
+	~Array();
+
+	T& operator[](unsigned int index) const;
+    unsigned int size() const;
+} ;
+
+template <typename T>
+Array<T>::Array() {
+    this->_size = 0;
+    this->_arr = nullptr ;
+}
+
+template <typename T>
+Array<T>::~Array() {
+	if (this->_arr)
+		delete []this->_arr;
+}
+
+template <typename T>
+Array<T>::Array(unsigned int n) : _size(n), _arr(new T[_size]()) {}
+
+template <typename T>
+Array<T>::Array(const Array& to_copy) : _arr(new T[to_copy._size]()), _size(to_copy._size) {
+		if (this->_arr) {
+			for (unsigned int i = 0; i < this->_size; ++i) {
+			    this->_arr[i] = to_copy._arr[i];
+			}
+		}
+		return ;
+}
+
+template <typename T>
+Array<T>& Array<T>::operator=(const Array& to_copy) {
+	if (this != &to_copy) {
+		delete[] this->_arr;
+		this->_size = to_copy._size;
+		this->_arr = new T[this->_size]();
+		for (unsigned int i = 0; i < this->_size; ++i) {
+			this->_arr[i] = to_copy._arr[i];
+		}
+	}
+	return *this;
+}
+
+template <typename T>
+unsigned int Array<T>::size() const {
+	return (this->_size);
+}
+
+template <typename T>
+T& Array<T>::operator[](unsigned int ind) const {
+	if (ind >= this->_size || ind < 0) {
+		throw std::out_of_range("Wrong index:-(");
+	}
+	return this->_arr[ind];
+}
+
+#endif
---
+++ CPP07/ex02/main.cpp
@@ -0,0 +1,41 @@
+#include "incl/Array.hpp"
+
+int main() {
+    unsigned int i;
+
+    Array<int> one;
+	std::cout << "Empty size: " << one.size() << std::endl;
+	try {
+		one[1] = 2;
+	}
+	catch (std::exception &e) {
+        std::cout << e.what() << std::endl;
+    }
+
+    Array<int> two(5);
+	std::cout << "Int size: " << two.size() << std::endl;
+	try {
+        for (i = 0; i < 5; ++i)
+    		two[i] = i;
+        for (i = 0; i < 5; ++i)
+    		std::cout << two[i] << " ";
+        std::cout << std::endl;
+	} 
+	catch (std::exception &e) {
+        std::cout << e.what() << std::endl;
+    }
+
+    Array<char> three(10);
+    std::cout << "Char size: " << three.size() << std::endl;
+    try {
+        for (i = 0; i < 5; ++i)
+    		two[i] = i + 97;
+        for (i = 0; i < 5; ++i)
+    		std::cout << two[i] << " ";
+        std::cout << std::endl;
+    } 
+    catch (std::exception &e) {
+        std::cout << e.what() << std::endl;
+    }
+    return (0);
+}
